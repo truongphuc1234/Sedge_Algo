c4739679c586c964485b2ecb9655b6e43db19956
Phuc Pham <truongphudn@gmail.com>
2023-12-02 15:04:02 +0700

    Init Project

---
+++ StdLib/StdRandom.cs
@@ -0,0 +1,18 @@
+﻿namespace StdLib;
+public class StdRandom
+{
+    private int seed;
+    private static readonly StdRandom stdRandom = new();
+
+    private StdRandom() { }
+
+    public static void SetSeed(int seed)
+    {
+        stdRandom.seed = seed;
+    }
+
+    public static double Random()
+    {
+        return new Random(stdRandom.seed).Next();
+    }
+}
